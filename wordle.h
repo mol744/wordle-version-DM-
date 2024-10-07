cf28fa80b1aa06bf668955ec9a8539134409bb4e
mol744 <camjfm744@gmail.com>
2024-10-07 17:51:31 -0300

    jordan

--- wordle.h
+++ wordle.h
@@ -2,4 +2,15 @@
 #define WORDLE_H
 
 
+typedef struct {
+    char arreglo[5];
+    char estado[5];
+}Interfaz;
+
+void inicio_juego(); //interfaz de juego
+
+int letra_presente(char c, int posicion, );
+
+Interfaz cambio_estado(Interfaz)
+
 #endif
